e159aa6d9efd576ba1ab2ed4b550d7858ac75c8a
Petersan19 <pedro.miranda@alunos.ifsuldeminas.edu.br>
2025-10-12 23:19:21 -0300

    Update descansoCurto.js

--- src/pages/descansoCurto.js
+++ src/pages/descansoCurto.js
@@ -1,24 +1,29 @@
 import { Cronometro } from "../components/cronometro";
 import { Botao } from "../components/botao";
-import { CircleHelp, CirclePlay, PauseCircle, SkipForward } from "lucide-react";
+import {
+  CircleHelp,
+  CirclePlay,
+  PauseCircle,
+  SkipForward,
+} from "lucide-react-native";
 import { Estados } from "../components/estados";
 import { Texto } from "../components/texto";
 import { StyleSheet, View } from "react-native";
 import { useCallback, useState } from "react";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { verificaContagem } from "../utils/verificaContagem";
 import { useFocusEffect } from "@react-navigation/native";
 
 export function DescansoCurto({ navigation }) {
   const [start, setStart] = useState(false);
-  const [nomeTarefaAtual, setNomeTarefaAtual] = useState('');
+  const [nomeTarefaAtual, setNomeTarefaAtual] = useState("");
 
   useFocusEffect(
     useCallback(() => {
       const carregarTarefa = async () => {
-        let nomeTarefa = await AsyncStorage.getItem('nomeTarefa');
+        let nomeTarefa = await AsyncStorage.getItem("nomeTarefa");
         if (!nomeTarefa || !nomeTarefa.trim()) {
-          nomeTarefa = '';
+          nomeTarefa = "";
         }
         setNomeTarefaAtual(nomeTarefa);
       };
@@ -33,61 +38,100 @@ export function DescansoCurto({ navigation }) {
 
   return (
     <View style={styles.container}>
-        <Cronometro color={'purple'} time={5} start={start} navigation={navigation} />
-        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-            <Botao color={'purple'} onPress={handleStart}>
-              {start ? <PauseCircle style={styles.icon} /> : <CirclePlay style={styles.icon} />}
-            </Botao>
-            <Botao color={'purple'} onPress={() => verificaContagem(navigation)}>
-                <SkipForward style={styles.iconNext}/>
-            </Botao>
-        </View>
-        <Estados color={'purple'} />
-        <Texto>Tempo de Descanso Curto</Texto>
-        <Texto>Tarefa: {nomeTarefaAtual}</Texto>
-        <Botao color={'purple'} >
-            <CircleHelp style={styles.iconQuestion} />
+      <Cronometro
+        color={"purple"}
+        time={5}
+        start={start}
+        navigation={navigation}
+      />
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <Botao color={"purple"} onPress={handleStart}>
+          {start ? (
+            <PauseCircle style={styles.icon} />
+          ) : (
+            <CirclePlay style={styles.icon} />
+          )}
         </Botao>
+        <Botao color={"purple"} onPress={() => verificaContagem(navigation)}>
+          <SkipForward style={styles.iconNext} />
+        </Botao>
+      </View>
+      <Estados color={"purple"} />
+      <Texto>Tempo de Descanso Curto</Texto>
+      <Texto>Tarefa: {nomeTarefaAtual}</Texto>
+      <Botao color={"purple"}>
+        <CircleHelp style={styles.iconQuestion} />
+      </Botao>
     </View>
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: '#3E2D7C',
+//   },
+//   input: {
+//     height: 250,
+//     width: 250,
+//     borderColor: 'gray',
+//     borderWidth: 2,
+//     margin: 20,
+//     paddingHorizontal: 15,
+//     borderRadius: 5,
+//     backgroundColor: 'white',
+//   },
+//   icon: {
+//     width: 40,
+//     height: 40,
+//     backgroundColor: 'white',
+//     color: '#3E2D7C',
+//     borderRadius: 60,
+//   },
+//   iconNext: {
+//     width: 40,
+//     height: 40,
+//     backgroundColor: '#3E2D7C',
+//     color: 'white',
+//     borderRadius: 60,
+//   },
+//   iconQuestion: {
+//     width: 25,
+//     height: 25,
+//     backgroundColor: 'white',
+//     color: '#3E2D7C',
+//     borderRadius: "100%",
+//   }
+// });
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#3E2D7C',
-  },
-  input: {
-    height: 250,
-    width: 250,
-    borderColor: 'gray',
-    borderWidth: 2,
-    margin: 20,
-    paddingHorizontal: 15,
-    borderRadius: 5,
-    backgroundColor: 'white',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#3E2D7C",
   },
   icon: {
     width: 40,
     height: 40,
-    backgroundColor: 'white',
-    color: '#3E2D7C',
-    borderRadius: 60,
+    backgroundColor: "white",
+    color: "#3E2D7C",
+    borderRadius: 20,
   },
   iconNext: {
     width: 40,
     height: 40,
-    backgroundColor: '#3E2D7C',
-    color: 'white',
-    borderRadius: 60,
+    backgroundColor: "#3E2D7C",
+    color: "white",
+    borderRadius: 20,
   },
   iconQuestion: {
     width: 25,
     height: 25,
-    backgroundColor: 'white',
-    color: '#3E2D7C',
-    borderRadius: "100%",
-  }
+    backgroundColor: "white",
+    color: "#3E2D7C",
+    borderRadius: 12.5,
+  },
 });
