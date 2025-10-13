2cf4be4c24dfafb86a21e3418f416b0bd5ed4cd7
Petersan19 <pedro.miranda@alunos.ifsuldeminas.edu.br>
2025-10-13 00:51:20 -0300

    Updating the marginTop of the text

--- src/components/texto.js
+++ src/components/texto.js
@@ -1,7 +1,17 @@
+// export function Texto({ children, style }) {
+//   return (
+//     <p style={{ ...style, color: 'white', fontSize: '25px' }}>
+//       {children}
+//     </p>
+//   );
+// }
+
+import { Text } from "react-native";
+
 export function Texto({ children, style }) {
   return (
-    <p style={{ ...style, color: 'white', fontSize: '25px' }}>
+    <Text style={{ ...style, color: "white", fontSize: 25, marginTop: 20 }}>
       {children}
-    </p>
+    </Text>
   );
 }
