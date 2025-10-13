5209dfe082a1daec51ac9569f50dc05160040658
Petersan19 <pedro.miranda@alunos.ifsuldeminas.edu.br>
2025-10-12 23:14:25 -0300

    Updating App.js
    Updating the code to compatible with React-Native

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import { View } from 'react-native-web';
-import Foco from './pages/foco';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet } from 'react-native';
-import { DescansoCurto } from './pages/descansoCurto';
-import { DescansoLongo } from './pages/descansoLongo';
+// import { View } from 'react-native-web';
+// import Foco from './pages/foco';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { StyleSheet } from 'react-native';
+// import { DescansoCurto } from './pages/descansoCurto';
+// import { DescansoLongo } from './pages/descansoLongo';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <NavigationContainer>
+//         <Stack.Navigator initialRoute="home" screenOptions={{ headerShown: false}} >
+//           <Stack.Screen name="home" component={Foco} />
+//           <Stack.Screen name="foco" component={Foco}  />
+//           <Stack.Screen name="descansoCurto" component={DescansoCurto} />
+//           <Stack.Screen name="descansoLongo" component={DescansoLongo} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </View>
+//   );
+// }
+
+// const Stack = createNativeStackNavigator(); // Criando a pilha de navegação
+
+// const styles = StyleSheet.create({
+//   container: {
+//     width: '100vw',
+//     height: '100vh',
+//   }
+// });
+
+import { View, Text } from "react-native";
+import Foco from "./pages/foco";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
+import { DescansoCurto } from "./pages/descansoCurto";
+import { DescansoLongo } from "./pages/descansoLongo";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        <Stack.Navigator initialRoute="home" screenOptions={{ headerShown: false}} >
+        <Stack.Navigator
+          initialRoute="home"
+          screenOptions={{ headerShown: false }}
+        >
           <Stack.Screen name="home" component={Foco} />
-          <Stack.Screen name="foco" component={Foco}  />
+          <Stack.Screen name="foco" component={Foco} />
           <Stack.Screen name="descansoCurto" component={DescansoCurto} />
           <Stack.Screen name="descansoLongo" component={DescansoLongo} />
         </Stack.Navigator>
@@ -21,11 +58,8 @@ export default function App() {
   );
 }
 
-const Stack = createNativeStackNavigator(); // Criando a pilha de navegação
-
 const styles = StyleSheet.create({
   container: {
-    width: '100vw',
-    height: '100vh',
-  }
+    flex: 1,
+  },
 });
